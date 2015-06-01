d7a9961f4fd8d94f4b1d8e0bf1ef5fb65378bce7
gilnakar <g.nakar@gmail.com>
2015-06-01 12:34:37 +0300

    jquery menues functionality tests

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 
+/*******************************************/
+/*jQuery*/
+/*******************************************/
+$(function () {
+
+/*Dropdown Menu*/
+
+    $(".action-list").hide();
+    $(".nav-section").hover(
+		function () {
+		    $(this).find(".action-list").slideDown();
+		    $(this).find(".arrow").toggleClass('rotate');
+		},
+		function () {
+		    $(this).find(".action-list").slideUp();
+		    $(this).find(".arrow").toggleClass('rotate-reset');
+	});
+
+/*Tabs Menu*/	
+	$(".tablist li").click(
+	 	function(){
+	 		var a_href = $(this).find("a").attr('href');
+	 		console.log(a_href);
+	 		window.hash =  a_href;
+	 		$(".tab-panel").hide();
+	 		$(".tabs").find(".tab").css("background-color","#444");
+	 		$(".tabs").find(".tab").css("color","white");
+
+	 		$(".tabs").find(a_href).slideToggle();
+	 		$(this).find("a").css("background-color"," #E6E6E6");
+	 		$(this).find("a").css("color"," black");
+	 	   
+	});
 
 
-		var cancel = UTILS.qs('.cancel');
-		console.log(cancel);
 
-		UTILS.addEvent(cancel, 'click', function(e) {
-			var form = UTILS.qs('#form1');
-			form.className = "hidden";
-			
-			
-			});
-		;
+
+});
 		
 
 
@@ -25,22 +51,22 @@
 window.addEventListener('load', init, false);
 
 var options = {
-	done: function (json){
-		console.log(json);
+    done: function (json) {
+        console.log(json);
         var notifications = UTILS.qs(".notifications");
-        if (json.notification){
+        if (json.notification) {
             var content = document.createTextNode(json.notification);
             notifications.appendChild(content);
         };
 
     },
-	fail: function (err){
-		console.log(err)
-	}
+    fail: function (err) {
+        console.log(err)
+    }
 }
 
-function init(){
-	UTILS.ajax('data/config.json', options);
+function init() {
+    UTILS.ajax('data/config.json', options);
 }
 
 
